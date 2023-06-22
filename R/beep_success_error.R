d439cb9c112ea25736cde527f25117275a008aed
rmflight <r.flight79@gmail.com>
2023-06-22 19:43:13 -0400

    adding ability to use ntfy for notification

--- R/beep_success_error.R
+++ R/beep_success_error.R
@@ -18,3 +18,27 @@ beep_success_error = function(expr, success = 4, error = 2)
     return(tmp)
   }
 }
+
+#' ntfy on success or error
+#' 
+#' Given an expression to evaluate, sends a ntfy message on error or success so you still know it is done.
+#' 
+#' @param expr the expression to evaluate
+#' @param success the message to send upon success
+#' @param error the message to send upon an error
+#'
+#' @export
+#' @return whatever should have been returned
+ntfy_success_error = function(expr, success = "It Worked!", error = "Better Check on Me.")
+{
+  if (!require("ntfy")) {
+    stop("ntfy is required to be setup.\nSee https://github.com/jonocarroll/ntfy")
+  }
+  tmp = try(expr)
+  if (inherits(tmp, "try-error")) {
+    ntfy::ntfy_send(error)
+  } else {
+    ntfy::ntfy_send(success)
+    return(tmp)
+  }
+}
