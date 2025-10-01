5926b0a6497e383e209686992099a9fbdd6f5669
rmflight <r.flight79@gmail.com>
2025-09-30 21:04:06 -0400

    updated to give a full roxygen templated function

--- R/analysis_helpers.R
+++ R/analysis_helpers.R
@@ -3,36 +3,49 @@
 #' A helper that provides a template for limma code to programmatically
 #' set up the differences one wants to extract.
 #'
+#' @examples
+#' ft_limma_code()
+#'
 #' @export
 #' @family 'Analysis'
 #' @return NULL, invisibly
 ft_limma_code = function() {
   cat(
-    '
-  # Assuming you are doing equivalent to a t-test,
-  # with two levels.
-  # The "var" here is which piece of the "info_df" contains
-  # the factors of interest.
-  #
-  # The order of the "fct" here will
-  # determine which one is used for reference (fct_ref)
-  # and which one is considered the experimental (fct_exp).
-  # 
-  # You can change them by changing their ordering here in the contrast
-  # vector.  
-  contrast = c("var", "fct_ref", "fct_exp")
-  
-  info_df[[contrast[1]]] = factor(info_df[[contrast[1]]], levels = contrast[c(2,3)]
-  
+    '#\' run limma
+#\'
+#\' Run limma code between two levels or factors.
+#\'
+#\' @param data_in a matrix of values, columns are samples
+#\' @param data_info data.frame of attributes
+#\' @param contrast the attribute, and factors
+#\'
+#\' @examples
+#\' data_in = matrix(rnorm(200), nrow = 10, ncol = 20)
+#\' rownames(data_in) = paste0("f", seq(1, 10))
+#\' colnames(data_in) = paste0("s", seq(1, 20))
+#\'
+#\' data_info = data.frame(sample_id = paste0("s", seq(1, 20)), group = rep(c("g1", "g2"), each = 10))
+#\' # g1 is the reference
+#\' contrast = c("group", "g2", "g1")
+#\'
+#\' run_limma(data_in, data_info, contrast)
+#\' # g2 is the reference
+#\' contrast = c("group", "g1", "g2")  
+run_limma = function(data_in, data_info, contrast) {
+  data_info[[contrast[1]]] = factor(
+    data_info[[contrast[1]]],
+    levels = contrast[c(2, 3)]
+  )
+
   design_matrix = model.matrix(
     as.formula(paste("~", contrast[1], sep = " ")),
-    data = sample_info
+    data = data_info
   )
   ncol_design = ncol(design_matrix)
   colnames(design_matrix)[ncol_design] = contrast[1]
 
-  use_data = use_data[, info_df$sample_id]
-  lm_fit = limma::lmFit(use_data, design_matrix)
+  data_in = data_in[, data_info$sample_id]
+  lm_fit = limma::lmFit(data_in, design_matrix)
   e_fit = limma::eBayes(lm_fit)
 
   results = limma::topTable(
@@ -43,6 +56,7 @@ ft_limma_code = function() {
   )
   results$feature_id = rownames(results)
   return(results)
+}
 '
   )
   return(invisible(NULL))
